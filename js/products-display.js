b5204dfd75f37176a81851a3cb19bb410b0dafbb
Kkkseniya <kenya020@mail.ru>
2023-05-01 02:12:02 +0300

    created function getData, made cards render, mobile adaptive

---
+++ js/products-display.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+  const cardsBox = document.querySelector(".articles__box");
+
+  //для вывода по хэштегам
+  const hashList = document.querySelector(".hash-list-section");
+  const articlesTitle = document.querySelector(".articles__title");
+
+  hashList.addEventListener("click", (e) => {
+    let elem = e.target;
+    if (elem.closest(".hash-list__item")) {
+      getData("hash", elem.textContent.toLowerCase());
+      // getData(elem.textContent.toLowerCase);
+    }
+  });
+
+  //для отрисовки карточек
+  const renderCards = (array) => {
+    cardsBox.innerHTML = "";
+
+    array.forEach((item) => {
+      const card = document.createElement("div");
+      card.classList.add("articles__card");
+      card.innerHTML = `      
+          <a href="/categories/article.html" class="toarticle"></a>
+          <div class="card-top">
+            <img class="card-top__img" src=${item.img}>
+            <p class="card-top__hash">${item.hash.join(", ")}</p>
+          </div>
+
+          <div class="card-bottom">
+            <span class="card-bottom__date">${item.date}</span>
+            <h2 class="card-bottom__title">${item.title}</h2>
+            <p class="card-bottom__description">${item.description}</p>
+          </div>`;
+
+      cardsBox.append(card);
+    });
+  };
+
+  // const getDataFront = (type) => {};
+
+  const getData = (opt, value) => {
+    fetch("../db/db.json")
+      .then((res) => res.json())
+      .then((dbObj) => {
+        //сортировка статей по дате
+        const array = dbObj.sort(function (a, b) {
+          let dateA = new Date(a.date),
+            dateB = new Date(b.date);
+          return dateB - dateA;
+        });
+
+        let forRender = [];
+        if (opt === "type") {
+          console.log("front");
+          forRender = array.filter((item) => item.type === value);
+          console.log(forRender);
+        } else {
+          forRender = array.filter((item) => item.hash.includes(value));
+        }
+
+        if (forRender.length > 0) {
+          if (opt === "type") {
+            articlesTitle.textContent = value[0].toUpperCase() + value.slice(1);
+          } else {
+            articlesTitle.textContent = value.toUpperCase();
+          }
+          articlesTitle.classList.remove("categories__title--mess");
+          renderCards(forRender);
+        } else {
+          cardsBox.innerHTML = "";
+          if (opt === "type") {
+            articlesTitle.textContent = "В данном разделе пока нет статей";
+            articlesTitle.classList.add("categories__title--mess");
+            //font-size: 26px;
+          } else {
+            articlesTitle.textContent = "По данному хэштегу пока нет статей";
+            articlesTitle.classList.add("categories__title--mess");
+          }
+        }
+      });
+  };
+
+  //два параметра - где искать, что искать
+  // getData(opt, value);
+
+  const currentUrl = window.location.pathname;
+  if (currentUrl.includes("frontend")) {
+    getData("type", "frontend");
+  } else {
+    getData("type", "backend");
+  }
+});
---
+++ js/products-main.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+  // const cardsBox = document.querySelector(".articles__box"); //categories-articles
+  const categoriesArticles = document.querySelector(".categories-articles");
+  const categoriesBlock = document.querySelector(".categories__block");
+
+  //для вывода по хэштегам
+  const hashList = document.querySelector(".hash-list-section");
+  const categoriesTitle = document.querySelector(".categories__title");
+
+  hashList.addEventListener("click", (e) => {
+    let elem = e.target;
+    if (elem.closest(".hash-list__item")) {
+      categoriesBlock.style.display = "none";
+      categoriesArticles.style.display = "grid";
+
+      getData(elem.textContent.toLowerCase());
+    }
+  });
+
+  //для отрисовки карточек
+  const renderCards = (array) => {
+    categoriesArticles.innerHTML = "";
+
+    array.forEach((item) => {
+      const card = document.createElement("div");
+      card.classList.add("articles__card");
+      card.innerHTML = `      
+          <a href="/categories/article.html" class="toarticle"></a>
+          <div class="card-top">
+            <img class="card-top__img" src=${item.img}>
+            <p class="card-top__hash">${item.hash.join(", ")}</p>
+          </div>
+
+          <div class="card-bottom">
+            <span class="card-bottom__date">${item.date}</span>
+            <h2 class="card-bottom__title">${item.title}</h2>
+            <p class="card-bottom__description">${item.description}</p>
+          </div>`;
+
+      categoriesArticles.append(card);
+    });
+  };
+
+  const getData = (attribute) => {
+    fetch("../db/db.json")
+      .then((res) => res.json())
+      .then((dbObj) => {
+        //сортировка статей по дате
+        const array = dbObj.sort(function (a, b) {
+          let dateA = new Date(a.date),
+            dateB = new Date(b.date);
+          return dateB - dateA;
+        });
+
+        const forRender = attribute ? array.filter((item) => item.hash.includes(attribute)) : array;
+
+        if (forRender.length > 0) {
+          if (attribute) {
+            categoriesTitle.textContent = attribute.toUpperCase();
+          }
+          // categoriesTitle.style.fontSize = "32px";
+          categoriesTitle.classList.remove("categories__title--mess");
+          renderCards(forRender);
+        } else {
+          categoriesArticles.innerHTML = "";
+          if (attribute) {
+            categoriesTitle.textContent = "По данному хэштегу пока нет статей";
+            // categoriesTitle.style.fontSize = "26px";
+            categoriesTitle.classList.add("categories__title--mess");
+          } else {
+            categoriesTitle.textContent = "В данном разделе пока нет статей";
+            categoriesTitle.classList.add("categories__title--mess");
+          }
+        }
+      });
+  };
+
+  getData();
+});
