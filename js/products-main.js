28a61c54a3f58946bffd33993e28cad0b9ea1020
Kkkseniya <61092282+Kkkseniya@users.noreply.github.com>
2023-06-07 00:35:48 +0300

    Update products-main.js

--- js/products-main.js
+++ js/products-main.js
@@ -203,7 +203,7 @@ $(document).ready(function () {
   };
 
   const getData = (opt, attribute) => {
-    fetch("https://kkkseniya.github.io/ksenya-webdev/db/db.json")
+    fetch("/ksenya-webdev/db/db.json")
       .then((res) => res.json())
       .then((dbObj) => {
         //сортировка статей по дате
